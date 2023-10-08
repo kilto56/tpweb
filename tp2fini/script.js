769af730c9030e8959b64c7502ec13d3c910386d
kilto56 <112411444+kilto56@users.noreply.github.com>
2023-10-08 15:24:56 +0200

    new uploads

---
+++ tp2fini/script.js
@@ -0,0 +1,10 @@
+let buttonAnnexe = document.querySelector(".annexe").children;
+
+buttonAnnexe[0].addEventListener("click", function htmljs() {
+    location.replace("js-html.html");
+});
+
+buttonAnnexe[1].addEventListener("click", function cssjs() {
+    location.replace("js-css.html");
+});
+
